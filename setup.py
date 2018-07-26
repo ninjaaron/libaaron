8310ed029bc18750b6ca246fdf267d8d6f08f1ab
Aaron Christianson <ninjaaron@gmail.com>
2018-07-26 12:56:00 +0200

    added contenttype of readme for pypi

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 package = 'libaaron'
-version = '0.10'
+version = '0.11'
 with open('README.rst') as fh:
     long_description = fh.read()
 
@@ -9,6 +9,7 @@ setup(name=package,
       version=version,
       description='trivial functions I like to pack along for various things',
       long_description=long_description,
+      long_description_content_type='text/x-rst',
       url='https://github.com/ninjaaron/libaaron',
       packages=['libaaron'],
       python_requires='>=3.5')
