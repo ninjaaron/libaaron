c60fe993ad44a0ab601ccf78440f104f4429d62b
Aaron Christianson <ninjaaron@gmail.com>
2018-10-13 14:07:54 +0200

    made lxml optional

--- libaaron/__init__.py
+++ libaaron/__init__.py
@@ -1,3 +1,6 @@
 from .libaaron import (
-        reify, cached, w, DotDict, flatten, quietinterrupt, PBytes,
-        lxml_little_iter)
+        reify, cached, w, DotDict, flatten, quietinterrupt, PBytes,)
+try:
+    from .libaaron import xml_little_iter
+except ImportError:
+    pass
