2f2a90906726af83fa48984202dfd682fdb5a27b
Aaron Christianson <ninjaaron@gmail.com>
2018-10-26 15:14:46 +0200

    updated aio for 3.7

--- libaaron/aio.py
+++ libaaron/aio.py
@@ -1,13 +1,16 @@
+import sys
 from asyncio import *
 proc = create_subprocess_exec
+major, minor, *_ = sys.version_info
 
+if (major, minor) < (3, 7):
 
-def run(coro):
-    # stole the idea from 3.7
-    loop = get_event_loop()
-    return loop.run_until_complete(coro)
+    def run(coro):
+        loop = get_event_loop()
+        return loop.run_until_complete(coro)
 
+    def spawn(coro):
+        return get_event_loop().create_task(coro)
 
-def spawn(coro):
-    # also again stole the idea from 3.7
-    return get_event_loop().create_task(coro)
+else:
+    spawn = create_task
